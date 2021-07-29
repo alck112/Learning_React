f399dec4e57f9aba2ccf937cecb1e625d7926c07
LCKPC <andyleung122@gmail.com>
2021-07-29 19:28:21 +0800

    counter app set up

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Counter(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Counter;
+
+export default Counter;
+
+export default Counter;
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
