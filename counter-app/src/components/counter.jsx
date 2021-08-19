d4d39aff1474048c5aa679412ae4ee080b0ad87e
LCKMac <>
2021-08-19 22:35:55 +0800

    add reset function and navbar

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -15,11 +15,7 @@ class Counter extends Component {
     //         tags: ["tag1","tag2","tag3"]
     //     };
     // }
-    state = {
-        value: this.props.counter.value,
-        // imageUrl: 'https://picsum.photos/200',
-        // tags: ["tag1","tag2","tag3"]
-    };
+
 
 
 
@@ -30,19 +26,16 @@ class Counter extends Component {
     //     return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
     // }
 
-    handleIncrement = () => {
-        this.setState({value: this.state.value + 1});
-    };
 
 
     getBadgeClasses = () => {
         let classes = "badge m-2 ";
-        classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
+        classes += this.props.counter.value === 0 ? "badge-warning" : "badge-primary";
         return classes;
     };
 
     formatCount = () => {
-        const {value} = this.state;
+        const {value} = this.props.counter;
         return value === 0 ? "Zero" : value;
     };
 
@@ -57,7 +50,7 @@ class Counter extends Component {
                 {/*<img src={this.state.imageUrl} alt=""/>*/}
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 {/*<h2>It is.</h2>*/}
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
                 <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -14,15 +14,32 @@ class Counters extends Component {
         };
     }
 
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters: counters});
+    }
+
     handleDelete = counterId => {
         const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters: counters});
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
         this.setState({counters: counters});
     };
 
     render() {
         return (
             <div>
-                {this.state.counters.map(counter => <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />)}
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm">Reset</button>
+                {this.state.counters.map(counter => <Counter key={counter.id} onIncrement={this.handleIncrement} onDelete={this.handleDelete} counter={counter} />)}
             </div>
         );
     }
