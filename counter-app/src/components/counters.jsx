9a3987f144f21c12c03237b1d26df480043adcc4
LCKPC <andyleung122@gmail.com>
2021-08-20 16:55:18 +0800

    add total counter number, lifted state to parennt component

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,44 +2,21 @@ import React, {Component} from 'react';
 import Counter from "./counter";
 
 class Counters extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            counters: [
-                { id:1, value: 4},
-                { id:2, value: 0},
-                { id:3, value: 0},
-                { id:4, value: 0}
-            ]
-        };
-    }
-
-    handleIncrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = {...counter};
-        counters[index].value++;
-        this.setState({counters: counters});
-    }
-
-    handleDelete = counterId => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({counters: counters});
-    };
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({counters: counters});
-    };
 
     render() {
         return (
             <div>
-                <button onClick={this.handleReset} className="btn btn-primary btn-sm">Reset</button>
-                {this.state.counters.map(counter => <Counter key={counter.id} onIncrement={this.handleIncrement} onDelete={this.handleDelete} counter={counter} />)}
+                <button onClick={this.props.onReset}
+                        className="btn btn-primary btn-sm"
+                >
+                    Reset
+                </button>
+                {this.props.counters.map(counter =>
+                    <Counter key={counter.id}
+                             onIncrement={this.props.onIncrement}
+                             onDelete={this.props.onDelete}
+                             counter={counter}
+                    />)}
             </div>
         );
     }
