dc108546b11d65ea8eb3ad5d8d99829fb8133d85
LCKPC <andyleung122@gmail.com>
2021-09-15 13:15:18 +0800

    react router

---
+++ start/router-app/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import {BrowserRouter as Router,
+    Switch,
+    Route,
+    Link} from "react-router-dom";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <div className="content">
+                    <Switch>
+                        <Route path="/products" component={Products}/>
+                        <Route path="/posts" component={Posts}/>
+                        <Route path="/admin" component={Dashboard}/>
+                        <Route path="/" exact component={Home}/>
+                    </Switch>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
