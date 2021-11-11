3474f814ffeb44377a7b6d4c40e10f48bd71a76f
LCKPC <andyleung122@gmail.com>
2021-11-11 20:55:06 +0800

    fisnished replacing fakeGenreService and fakeMovieService with genreService  and movieService

--- vidly/src/services/genreService.js
+++ vidly/src/services/genreService.js
@@ -1,5 +1,11 @@
-import http from "./httpService"
+import http from "./httpService";
+// using object destructuring
+import {apiUrl} from "../config.json";
+// or traditional naming to import
+// import config from "../config.json";
 
 export function getGenres() {
-    return http.get("http://localhost:3900/api/genres");
+    return http.get(apiUrl + "/genres");
+    // return http.get(config.apiUrl + "/genres");
+    // return http.get(`${apiUrl}/genres`);
 }
